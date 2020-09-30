63061378f49924444aebc919ceb697c95926acf3
brubriele <brunagabriele.paula@gmail.com>
2020-09-30 18:46:13 -0300

    Create routes for application

--- app.js
+++ app.js
@@ -2,17 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const CurriculoController = require('./controllers/curriculo-controller');
 
 // Setup view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Meu primeiro servidor Express',
-    version: '0.0.0',
-  });
-});
+app.use('/', require('./routes'));
+app.use('/', require('./routes/curriculo'));
 
 app.listen(port, (err) => {
   console.log(`Server is listening on ${port}`);
---
+++ models/curriculo-model.js
@@ -0,0 +1,31 @@
+const data = {
+  title: 'Meu currículo',
+  name: 'Bruna Gabriele de Paula',
+  profession: 'Desenvolvedora Front-end',
+  description: '...',
+  experience: [
+    {
+      company: 'Gauge',
+      office: 'Analista Adobe AEM | Front-end',
+      description: '...',
+    },
+    {
+      company: 'everis',
+      office: 'Solutions Assistant | Front-end',
+      description: '...',
+    },
+  ],
+  education: [
+    {
+      institution: 'Universidade Federal de São Paulo/EFLCH',
+      description: 'Ciências Sociais',
+    },
+    {
+      institution: 'Laboratoria',
+      description: 'Bootcamp Desenvolvimento Front-end UX/UI',
+    },
+  ],
+  skills: ['frontend', 'acessibilidade web', 'cms'],
+};
+
+module.exports = data;
