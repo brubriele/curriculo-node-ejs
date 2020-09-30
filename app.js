879a789d13b786d073cc120873c3f069a25fff22
brubriele <brunagabriele.paula@gmail.com>
2020-09-30 17:45:24 -0300

    Creates structure for templates and starts nodemon dependency

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const port = 3000;
 const app = express();
+const port = 3000;
+const path = require('path');
 
-app.get('/', (rew, res, next) => {
-  res.send({
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res, next) {
+  res.render('index', {
     title: 'Meu primeiro servidor Express',
     version: '0.0.0',
   });
 });
 
-app.listen(port, (ett) => {
+app.listen(port, (err) => {
   console.log(`Server is listening on ${port}`);
 });
