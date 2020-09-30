7da7c8fca2a144f959e7affab8f31ca90bd61d3d
brubriele <brunagabriele.paula@gmail.com>
2020-09-30 18:47:37 -0300

    Removes unnecessary code

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const CurriculoController = require('./controllers/curriculo-controller');
 
 // Setup view engine
 app.set('views', path.join(__dirname, 'views'));
