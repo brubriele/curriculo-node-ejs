2ced6ef5d3519a673cb30acdf539071222bab90a
brubriele <brunagabriele.paula@gmail.com>
2020-10-01 10:49:44 -0300

    Handling errors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const createError = require('http-errors');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Setup view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -12,4 +15,19 @@ app.use('/', require('./routes/curriculo'));
 
 app.listen(port, (err) => {
   console.log(`Server is listening on ${port}`);
+});
+
+// Handling errors
+// 404
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
 });
