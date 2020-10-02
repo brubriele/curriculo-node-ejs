a5013d510b65acf3696b612addd2a5c84e631ce6
brubriele <brunagabriele.paula@gmail.com>
2020-10-01 23:52:20 -0300

    style: cards estructure, style composition and animations

--- models/curriculo-model.js
+++ models/curriculo-model.js
@@ -18,19 +18,29 @@ const data = {
 
   projects: [
     {
-      name: 'Projeto A',
-      description: '.....',
-      repository: 'https://github.com/brubriele/lib-scale-chord-convert',
+      name: 'E-Commerce FASHIONISTA',
+      description:
+        'Desenvolvimento de principais funcionalidades de um e-commerce de moda feminina, o Fashionista, garantindo a melhor experiência possível para os usuários interagirem com os produtos.',
+      repository: 'https://github.com/brubriele/aceleracao-react',
     },
     {
       name: 'Instagram Réplica',
-      description: '.....',
+      description:
+        'Desenvolvimento uma versão mobile first do Instagram utilizando apenas HTML5 e CSS3.',
       repository: 'https://brubriele.github.io/react-women-chal-2/public/',
     },
     {
-      name: 'Projeto C',
-      description: '.....',
+      name: 'Conversor de Escalas Musicais',
+      description:
+        'Biblioteca npm para transposição de escalas Maior, Menor, Pentatônica, Pentatônica Maior, Menor Melódica, Menor Harmônica, Menor de Blues em diferentes tonalidades.',
       repository: 'https://github.com/brubriele/lib-scale-chord-convert',
+    },
+
+    {
+      name: 'Rede de Apoio - RAH',
+      description:
+        'Desenvolvimento de aplicativo Web responsivo da Rede de Apoio nas e das Periferias (RAH), uma iniciativa dos movimentos negros e periféricos do munícipio de São Paulo para realizar ações emergências de forma conjunta e organizada. ',
+      repository: 'https://github.com/brubriele/GT-WEB-RAH',
     },
   ],
   education: [
