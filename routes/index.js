1039359e3c31b1998e410286807e3a1ae52b7d66
brubriele <brunagabriele.paula@gmail.com>
2020-10-04 05:19:25 -0300

    style-responsive: menu mobile and link structure, requires implementation for links via route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.render('index', {
-    title: 'Meu primeiro servidor Express',
+    title: 'BrunaGPaula',
+    navItems: ['Perfil', 'Carreira', 'Projetos'],
+    linkItems: [
+      '/curriculo#porftolio',
+      '/curriculo#porftolio',
+      '/curriculo#porftolio',
+    ],
     version: '0.0.0',
   });
 });
