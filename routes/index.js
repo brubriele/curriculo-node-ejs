f19eaa16c57cd88c1d066b0602fc070a828324a5
brubriele <brunagabriele.paula@gmail.com>
2020-10-05 12:41:59 -0300

    animation: includes animation words in Home

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,21 @@ router.get('/', (req, res, next) => {
       '/curriculo#porftolio',
       '/curriculo#porftolio',
     ],
+    animationItems: ['Perfil', 'Carreira', 'Projetos'],
     version: '0.0.0',
   });
 });
+
+// var jsdom = require('jsdom');
+// const { JSDOM } = jsdom;
+// const { window } = new JSDOM();
+// const { document } = new JSDOM('').window;
+// global.document = document;
+// var $ = require('jquery')(window);
+
+// $(document).ready(function () {
+//   let teste = $('.box-');
+//   console.log(teste);
+// });
 
 module.exports = router;
