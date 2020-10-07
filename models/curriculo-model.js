c41225a2769bedeef775fe26cacaa2ae03457afb
brubriele <brunagabriele.paula@gmail.com>
2020-10-06 21:07:39 -0300

    :art: Adjust stack description

--- models/curriculo-model.js
+++ models/curriculo-model.js
@@ -21,7 +21,7 @@ const data = {
     {
       name: 'E-Commerce FASHIONISTA',
       description:
-        'Um E-commerce de moda, o Fashionista, garantindo a melhor experiência possível para os usuários interagirem com os produtos.',
+        'E-commerce de moda, o Fashionista, garantindo a melhor experiência possível para os usuários interagirem com os produtos.',
       repository: 'https://github.com/brubriele/aceleracao-react',
       stack1: 'fab fa-react',
       stack2: 'fab fa-js-square',
@@ -39,7 +39,7 @@ const data = {
     {
       name: 'Conversor de Escalas Musicais',
       description:
-        'Biblioteca npm para transposição de escalas Maior, Menor, Pentatônica, Pentatônica Maior, Menor Melódica, Menor Harmônica, Menor de Blues em diferentes tonalidades.',
+        'Biblioteca de transposição de escalas Maior, Menor, Pentatônica, Pentatônica Maior, Menor Melódica, Menor Harmônica, Menor de Blues em diferentes tonalidades.',
       repository: 'https://github.com/brubriele/lib-scale-chord-convert',
       stack1: 'fab fa-npm',
       stack2: 'fab fa-js-square',
@@ -48,8 +48,11 @@ const data = {
     {
       name: 'Rede de Apoio - RAH',
       description:
-        'Desenvolvimento de aplicativo Web responsivo da Rede de Apoio nas e das Periferias (RAH), uma iniciativa dos movimentos negros e periféricos do munícipio de São Paulo para realizar ações emergências de forma conjunta e organizada. ',
+        'Aplicativo Web responsivo da Rede de Apoio nas e das Periferias (RAH), uma iniciativa dos movimentos negros e periféricos do munícipio de São Paulo. ',
       repository: 'https://github.com/brubriele/GT-WEB-RAH',
+      stack1: 'fab fa-react',
+      stack2: 'fab fa-js-square',
+      stack3: 'fab fa-sass',
     },
   ],
   education: [
